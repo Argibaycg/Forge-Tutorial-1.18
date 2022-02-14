53d056fa9c204a52e5e0db1d4936940876fdf9c5
Gaston <argibaycg@gmail.com>
2022-02-13 22:49:01 -0300

    add - two items

--- Forge-Tutorial 1.18/src/main/java/net/gaston/examplemod/TutorialMod.java
+++ Forge-Tutorial 1.18/src/main/java/net/gaston/examplemod/TutorialMod.java
@@ -1,9 +1,11 @@
 package net.gaston.examplemod;
 
+import net.gaston.examplemod.Item.ModItems;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -18,16 +20,17 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(TutorialMod.EXAMPLEMOD)
+@Mod(TutorialMod.MOD_ID)
 public class TutorialMod
 {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
-    public static final String EXAMPLEMOD = "tutorialmod";
+    public static final String MOD_ID = "tutorialmod";
 
     public TutorialMod() {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        ModItems.register(eventBus);
+        eventBus.addListener(this::setup);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
